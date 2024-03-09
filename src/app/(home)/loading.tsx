1b3885a132709c0303e6e51662d294d208aadc57
thanhhuy44 <thhuy444111@gmail.com>
2024-03-10 01:15:10 +0700

    lint code and remove yarn

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -14,7 +14,7 @@ export default function Page() {
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-2 xl:gap-3">
         {Array.from({ length: 20 }, (_, index) => `Item ${index + 1}`).map(
           (_, index) => (
-            <div className="flex flex-col gap-y-1">
+            <div key={index} className="flex flex-col gap-y-1">
               <Skeleton key={index} className="w-full aspect-[3/4]" />
               <Skeleton count={2} />
             </div>
